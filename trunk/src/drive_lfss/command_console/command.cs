a74edeb77d55817fae7342d23f66a8399dcb3736
greenseed <greenseed@ec293b73-d7cd-6945-bb04-10334b7ef37c>
2008-07-28 06:23:13 +0000

    Added: Console Command "announce" usage: "announce #serverId $message", patch provided by Forcemagic.

--- trunk/src/drive_lfss/command_console/command.cs
+++ trunk/src/drive_lfss/command_console/command.cs
@@ -18,6 +18,10 @@
 namespace Drive_LFSS.CommandConsole_
 {
     using Drive_LFSS;
+    using Drive_LFSS.Session_;
+    using System.Collections.Generic;
+    using System;
+    using Drive_LFSS.Packet_;
     //using System.Collections.Generic;
     //using System.Text;
 
@@ -25,15 +29,48 @@ namespace Drive_LFSS.CommandConsole_
     {
         public static void Exec(string _commandText)
         {
-            switch (_commandText)
+            string[] args = _commandText.Split(new string[] { " " }, 2, StringSplitOptions.RemoveEmptyEntries);
+
+            switch (args[0])
             {
+                case "announce": Announce(args[1]); break;
                 case "exit": Exit(); break;
                 default:
                 {
-                    if (_commandText != " ")
-                        Program.log.error("Unknow Command: " + _commandText + "\r\n");
+                    Program.log.error("Unknow Command: " + _commandText + "\r\n");
                     break;
                 }
+            }
+        }
+        private static void Announce(string _commandText)
+        {
+            string[] args = _commandText.Split(new string[] { " " }, 2, StringSplitOptions.RemoveEmptyEntries);
+
+            if (args.Length != 2)
+            {
+                Program.log.normal("Command Announce, Syntax Error.\r\n  Usage:\r\n    announce #serverId $Message\r\n");
+                return;
+            }
+
+            if (args[0] == "*")
+            {
+                foreach (KeyValuePair<ushort, Drive_LFSS.Session_.SessionList.SessionStruct> keyPair in SessionList.sessionList)
+                    SessionList.sessionList[keyPair.Key].session.AddToTcpSendingQueud(new Packet(Packet_Size.PACKET_SIZE_MST, Packet_Type.PACKET_MST_SEND_NORMAL_CHAT, new PacketMST(args[1])));
+            }
+            else
+            {
+                ushort serverId;
+                try { serverId = Convert.ToUInt16(args[0]); }
+                catch (Exception _exception)
+                {
+                    Program.log.normal("Command Announce, Syntax Error.\r\n  Usage:\r\n    announce #serverId $Message\r\n");
+                    return;
+                }
+
+                if (SessionList.sessionList.ContainsKey(serverId))
+                    SessionList.sessionList[serverId].session.AddToTcpSendingQueud(new Packet(Packet_Size.PACKET_SIZE_MST, Packet_Type.PACKET_MST_SEND_NORMAL_CHAT, new PacketMST(args[1])));
+                else
+                    Program.log.command("Command Announce, ServerId Not Found, Server Requested was: " + args[0] + "\r\n");
             }
         }
         private static void Exit()
@@ -41,6 +78,5 @@ namespace Drive_LFSS.CommandConsole_
             Program.log.normal("Exiting Requested, Please Wait For All Thread Too Close...\n\r");
             Program.Exit();
         }
-
     }
 }
