9b287edc91cbff908bb52150de7a5513f54bd8e8
greenseed <greenseed@ec293b73-d7cd-6945-bb04-10334b7ef37c>
2008-10-26 10:47:19 +0000

    Added: the last .sql patch file, no more needed since there is no more public release.

---
+++ trunk/sql/mysql/update/229_gui_template.sql
@@ -0,0 +1,6 @@
+-- Update the Rank gui
+UPDATE `gui_template` SET`text`= '^7Top20 ^2is the 20 Best Driver for this Track/Car.
+^7Search ^2search for a Track/Car/Driver rank.
+^7Current ^2show rank for all online drivers by Track/Car.
+^6Blue means worst^2, ^3Yellow means Better^2, ^7White means event';
+WHERE `entry` = 5;
